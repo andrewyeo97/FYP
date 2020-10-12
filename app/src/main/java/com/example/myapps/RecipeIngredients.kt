200a90e1ff1575680a146ecbbbc6f8d205b41b73
NdrwYeo <andrewyph-wm17@student.tarc.edu.my>
2020-10-12 16:16:42 +0800

    commit

---
+++ app/src/main/java/com/example/myapps/RecipeIngredients.kt
@@ -0,0 +1,12 @@
+package com.example.myapps
+import java.util.*
+
+class RecipeIngredients {
+
+    var recipeID: String = ""
+    var ingredientID: String = ""
+    var ingredientName: String = ""
+    var quantity: Int = 0
+    var description: String = ""
+
+}
